99f08f08f649d4f7172f4bfade0e2aa427a5808d
Markus Pint <markuspint@hotmail.com>
2015-09-16 19:23:43 +0300

    apiRoot https instead of http

--- tasks/browsersnapsy.js
+++ tasks/browsersnapsy.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         user: '',
         token: ''
       }),
-      apiRoot = 'http://www.browserstack.com/screenshots',
+      apiRoot = 'https://www.browserstack.com/screenshots',
       initializeTunnel,
       closeTunnel,
       tunnel,
